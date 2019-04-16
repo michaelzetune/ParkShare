7f8f4f1097ef394e06d0bd76b28e73de02aa45bb
Michael Zetune <michael@zetune.com>
2019-04-15 20:28:27 -0500

    Add Parse and Podfile, update AppDelegate file

--- ParkShare/ParkShare/AppDelegate.swift
+++ ParkShare/ParkShare/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,8 +17,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        // Parse setup
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "park-share-app"
+                configuration.server = "http://park-share-app.herokuapp.com/parse"
+            })
+        )
+        
+        if PFUser.current() != nil {
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            let searchTableViewController = main.instantiateViewController(withIdentifier: "SearchTableViewController")
+            window?.rootViewController = searchTableViewController
+        }
+        
+        
+        
         return true
     }
+
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
