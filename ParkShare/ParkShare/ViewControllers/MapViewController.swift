1cd9d2161ef51b9fb579984a0b97b808647986d8
Michael Zetune <michael@zetune.com>
2019-05-07 13:25:39 -0500

    Add map view with current location to feed

--- ParkShare/ParkShare/ViewControllers/MapViewController.swift
+++ ParkShare/ParkShare/ViewControllers/MapViewController.swift
@@ -10,33 +10,71 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
-    @IBOutlet weak var Map: MKMapView!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let manager = CLLocationManager()
+    
+//    fileprivate var annotation: MKAnnotation!
+    fileprivate var locationManager: CLLocationManager!
+    fileprivate var isCurrentLocation: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let location = CLLocationCoordinate2DMake(30.285172, -97.735743)
-                
-        let annotation = MKPointAnnotation()
         
-        annotation.coordinate = location
-        annotation.title = "2200 Neuces St"
-        annotation.subtitle = "$1500"
-        Map.addAnnotation(annotation)
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
+        mapView.mapType = .standard
     }
     
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+//        activityIndicator.center = self.view.center
     }
-    */
-
+    
+    @IBAction func getLocationButtonPressed(_ sender: Any) {
+        if (CLLocationManager.locationServicesEnabled()) {
+            if locationManager == nil {
+                locationManager = CLLocationManager()
+            }
+            locationManager?.requestWhenInUseAuthorization()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+            isCurrentLocation = true
+        }
+    }
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if !isCurrentLocation {
+            return
+        }
+        
+        isCurrentLocation = false
+        
+        let location = locations.last
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        self.mapView.setRegion(region, animated: true)
+        
+        if self.mapView.annotations.count != 0 {
+            self.mapView.removeAnnotation(self.mapView.annotations[0])
+        }
+        
+//        let pointAnnotation = MKPointAnnotation()
+//        pointAnnotation.coordinate = location!.coordinate
+//        pointAnnotation.title = ""
+//        mapView.addAnnotation(pointAnnotation)
+//
+        self.mapView.showsUserLocation = true
+    }
 }
+
